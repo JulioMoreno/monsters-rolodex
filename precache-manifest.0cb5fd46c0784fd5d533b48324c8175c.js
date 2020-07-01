d798112b7e6f85f3d93ca9b40d33619a8387c76d
JulioMoreno <jc.moreno.guillen@gmail.com>
2020-07-01 18:19:41 -0500

    Updates

---
+++ precache-manifest.0cb5fd46c0784fd5d533b48324c8175c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff48ecdff83b0b2e2889f3c45e5ba38",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "01754971597b326bfcbe",
+    "url": "/monsters-rolodex/static/css/main.8fa2999c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01754971597b326bfcbe",
+    "url": "/monsters-rolodex/static/js/main.7fe67684.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
